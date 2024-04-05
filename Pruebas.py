e90e75ec2ef26e87c24f4a5c10e2d7b36cca444f
Nico2402 <nicolascedeno@usantotomas.edu.co>
2024-04-04 23:41:45 -0500

    tercero

--- Pruebas.py
+++ Pruebas.py
@@ -7,8 +7,6 @@ Linear = LinearTopo(k=4)
 
 net = Mininet(topo=Linear) 
 
-c0 = RemoteController (name='C0',controller=RemoteController, port=6653,  ip= contip) 
-
 net.start() 
 net.pingAll() 
 net.stop() 
