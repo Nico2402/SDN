ae468d2bd5636deb78cf1ac7436f77a4fb9df472
Ubuntu <ubuntu@ip-172-31-31-28.ec2.internal>
2024-05-02 03:56:53 +0000

    cambio

--- mininet_remote_controller.py
+++ mininet_remote_controller.py
@@ -17,6 +17,7 @@ def myNetwork():
                    ipBase='10.0.0.0/8')
 
     info( '*** Adding controller\n' )
+
     c0=net.addController(name='c0',
                       controller=RemoteController,
                       ip='172.17.0.2',
@@ -27,8 +28,8 @@ def myNetwork():
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch, protocols=["OpenFlow10"])
     s2 = net.addSwitch('s2', cls=OVSKernelSwitch, protocols=["OpenFlow10"])
     s3 = net.addSwitch('s3', cls=OVSKernelSwitch, protocols=["OpenFlow10"])
+    s4 = net.addSwitch('s4', cls=OVSKernelSwitch, protocols=["OpenFlow10"])
     # s1 = net.addSwitch('s1', cls=OVSKernelSwitch, protocols=["OpenFlow10,OpenFlow13"])
-    # s2 = net.addSwitch('s2', cls=OVSKernelSwitch, protocols=["OpenFlow10,OpenFlow13"])
 
     info( '*** Add hosts\n')
     h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
@@ -37,17 +38,23 @@ def myNetwork():
     h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None)
     h5 = net.addHost('h5', cls=Host, ip='10.0.0.5', defaultRoute=None)
     h6 = net.addHost('h6', cls=Host, ip='10.0.0.6', defaultRoute=None)
+    h7 = net.addHost('h7', cls=Host, ip='10.0.0.6', defaultRoute=None)
 
     info( '*** Add links\n')
+
     net.addLink(s1, s2)
     net.addLink(s1, s3)
     net.addLink(s2, s3)
+    net.addLink(s1, s4)
+    net.addLink(s2, s4)
+    net.addLink(s3, s4)
     net.addLink(h1, s1)
     net.addLink(h2, s1)
     net.addLink(h3, s2)
     net.addLink(h4, s3)
     net.addLink(h5, s3)
     net.addLink(h6, s3)
+    net.addLink(h7, s4)
 
     info( '*** Starting network\n')
     net.build()
@@ -60,6 +67,7 @@ def myNetwork():
     net.get('s1').start([c0])
     net.get('s2').start([c0])
     net.get('s3').start([c0])
+    net.get('s4').start([c0])
 
     info( '*** Post configure switches and hosts\n')
 
