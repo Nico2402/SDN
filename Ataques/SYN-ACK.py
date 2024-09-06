0b0075994d5fae94a2178f0535747b5c6ff576d0
Ubuntu <ubuntu@ip-172-31-21-180.ec2.internal>
2024-09-06 03:33:06 +0000

    1st try PortMirror

--- Ataques/SYN-ACK.py
+++ Ataques/SYN-ACK.py
@@ -2,15 +2,57 @@ from scapy.all import *
 import threading
 import time
 
-def send_tcp_ack(ip, port):
+from pypacker.layer12 import ethernet
+from pypacker.layer3 import ip
+from pypacker.layer4 import tcp
+from pypacker.pypacker import Packer
+import socket
+
+def create_syn_packet(src_ip, dst_ip, dst_port):
+
+    source_ports = list(range(1024, 65535))
+    sport = random.choice(source_ports)
+    eth = ethernet.Ethernet(dst='ff:ff:ff:ff:ff:ff', src='52:6f:7b:49:78:20', type=0x0800)
+    ip_pkt = ip.IP(src=src_ip, dst=dst_ip)
+    tcp_pkt = tcp.TCP(sport=sport, dport=dst_port, flags='S', seq=1000)
+    pkt = eth / ip_pkt / tcp_pkt
+    return pkt
+
+def send_packet(packet, iface):
+    # Crear un socket raw
+    sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
+    
+    # Configurar la interfaz
+    sock.bind((iface, 0))
+
+    # Enviar el paquete
+    sock.send(packet.bytes)
+    print("Paquete SYN enviado!")
+
+
+if __name__ == "__main__":
+    source_ip= "10.0.0.2"
+    target_ip = "10.0.0.1"  # IP del servidor de prueba
+    destination_port = 80      # Puerto del servidor de prueba
+    #for i in range(0):
+    syn_packet = create_syn_packet(source_ip, destination_ip,destination_port)
+    send_packet(syn_packet, 'h2-eth0')
+
+
+
+
+#--------------------------------------------------------------------------------------------------------------
+
+
+
     # Crear el paquete SYN para iniciar la conexión
     #source_ports = list(range(1024, 65535))
     #sport = random.choice(source_ports)
     
-    syn = IP(dst=ip)/TCP(sport=1024, dport=port, flags='S')
+    #syn = IP(dst=ip)/TCP(sport=1024, dport=port, flags='S')
     #send(syn,timeout=1000)
-    time.sleep(2)
-    syn_ack = sr1(syn, timeout=1000, verbose=0)  # Enviar y recibir el SYN-ACK
+    #time.sleep(2)
+    #syn_ack = sr1(syn, timeout=1000, verbose=0)  # Enviar y recibir el SYN-ACK
 
     #if syn_ack is None:
     #    print(f"Conexión a {ip}:{port} falló al recibir SYN-ACK.")
@@ -42,11 +84,7 @@ def send_tcp_ack(ip, port):
     #else:
     #    print(f"No se recibió un SYN-ACK válido desde {ip}:{port}")
 
-if __name__ == "__main__":
-    target_ip = "10.0.0.1"  # IP del servidor de prueba
-    target_port = 80      # Puerto del servidor de prueba
-    #for i in range(0):
-    send_tcp_ack(target_ip, target_port)
+
     # Crea múltiples hilos para simular múltiples conexiones simultáneas
     #for i in range(1):  # Ajusta el número de hilos según sea necesario
      #   thread = threading.Thread(target=send_tcp_ack, args=(target_ip, target_port))
