989ab8401244dd7ca0b2c6068274a7ef33a309d9
Ubuntu <ubuntu@ip-172-31-21-180.ec2.internal>
2024-09-22 18:02:14 +0000

    prueba doble mensaje

--- Pruebas/TestTCP.py
+++ Pruebas/TestTCP.py
@@ -3,4 +3,4 @@ from scapy.all import *
 ip = IP(src="10.0.0.2", dst="10.0.0.1")
 tcp = TCP(sport=RandShort(), dport=80, flags="S")
 pkt = ip/tcp
-sendp(pkt, iface="h1-eth0", verbose=False)
+sendp(pkt, iface="h2-eth0", verbose=False)
