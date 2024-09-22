5f9b92898407ea0df99dff605029a2211e40d804
Ubuntu <ubuntu@ip-172-31-21-180.ec2.internal>
2024-09-22 18:07:47 +0000

    prueba doble mensaje

--- Pruebas/TestTCP.py
+++ Pruebas/TestTCP.py
@@ -1,6 +1,11 @@
 from scapy.all import *
 
+# Enviar el SYN
 ip = IP(src="10.0.0.2", dst="10.0.0.1")
-tcp = TCP(sport=RandShort(), dport=80, flags="S")
-pkt = ip/tcp
-send(pkt, iface="h2-eth0", verbose=False)
+tcp_syn = TCP(sport=RandShort(), dport=80, flags="S", seq=0)
+syn_ack = sr1(ip/tcp_syn, timeout=10)
+
+# Enviar el ACK para completar el handshake
+if syn_ack:
+    tcp_ack = TCP(sport=syn_ack.dport, dport=80, flags="A", seq=syn_ack.ack, ack=syn_ack.seq + 1)
+    send(ip/tcp_ack)
